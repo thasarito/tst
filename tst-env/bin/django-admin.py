4f582c8b8a1e63bb5572a88d2845d41fc32001c0
thasarito <terthasarit@gmail.com>
2018-11-15 05:40:12 +0000

    add re

---
+++ tst-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/thasarito/tst-repo/tst-env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- tstdjango/tstdjango/settings.py
+++ tstdjango/tstdjango/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '809i45e+d)v^4cgui!jn@u!x_c#c=-&7i1ot0&$t#*w7(q(in='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
